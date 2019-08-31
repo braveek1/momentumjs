21afd16330921a9aab231204f978c049b631a5d6
braveek1 <braveek1@gmail.com>
2019-08-31 10:25:29 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const braveInfo = {
+    name : "yongki",
+    favoriteFood : "kimchi",
+    favoriteMovie : [
+        {
+            name : "oldBoy",
+            age : "19"
+        },
+        {
+            name : "lionKing",
+            age : "all"
+        }
+    ]
+}
+
+console.log(braveInfo);
